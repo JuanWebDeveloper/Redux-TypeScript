71595d33d86d64149fa12fce7ec0e2290f4e1a10
Juan Vargas <juandeveloper19@gmail.com>
2022-07-09 19:39:12 -0500

    Configuracion para mostrar el estado y para  mutarlo por medio de las acciones

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-import Store from './store/store';
-import { counterReducer } from './reducers/counter.reducer';
-import { incrementAction } from './actions/operations.actions';
+import Store from './store/store.js';
+import { counterReducer } from './reducers/counter.reducer.js';
+import { decrementAction, divideAction, incrementAction, multiplyAction, resetAction } from './actions/operations.actions.js';
 const store = new Store(counterReducer, 10);
-console.log(store.getState());
-store.dispatch(incrementAction);
-console.log(store.getState());
+const count = document.getElementById('count');
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+count.innerHTML = store.getState();
+increment.addEventListener('click', () => {
+  store.dispatch(incrementAction);
+  count.innerHTML = store.getState();
+});
+decrement.addEventListener('click', () => {
+  store.dispatch(decrementAction);
+  count.innerHTML = store.getState();
+});
+reset.addEventListener('click', () => {
+  store.dispatch(resetAction);
+  count.innerHTML = store.getState();
+});
+multiply.addEventListener('click', () => {
+  store.dispatch(multiplyAction);
+  count.innerHTML = store.getState();
+});
+divide.addEventListener('click', () => {
+  store.dispatch(divideAction);
+  count.innerHTML = store.getState();
+});
--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,35 @@ import { decrementAction, divideAction, incrementAction, multiplyAction, resetAc
 
 const store = new Store(counterReducer, 10);
 
-console.log(store.getState());
+// Elementos HTML
+const count: any = document.getElementById('count');
+const increment: any = document.getElementById('increment');
+const decrement: any = document.getElementById('decrement');
+const reset: any = document.getElementById('reset');
+const multiply: any = document.getElementById('multiply');
+const divide: any = document.getElementById('divide');
 
-store.dispatch(incrementAction);
-console.log(store.getState());
+// Mostrar el contador en el HTML
+count.innerHTML = store.getState();
+
+// Eventos de click para mutar el estado del store (el contador).
+increment.addEventListener('click', () => {
+  store.dispatch(incrementAction);
+  count.innerHTML = store.getState();
+});
+decrement.addEventListener('click', () => {
+  store.dispatch(decrementAction);
+  count.innerHTML = store.getState();
+});
+reset.addEventListener('click', () => {
+  store.dispatch(resetAction);
+  count.innerHTML = store.getState();
+});
+multiply.addEventListener('click', () => {
+  store.dispatch(multiplyAction);
+  count.innerHTML = store.getState();
+});
+divide.addEventListener('click', () => {
+  store.dispatch(divideAction);
+  count.innerHTML = store.getState();
+});
